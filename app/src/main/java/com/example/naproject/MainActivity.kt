d26cff9b6df45b4ced4b932803e492f9e15da163
Pussa <45294408+Pussa@users.noreply.github.com>
2020-12-08 20:14:19 +0500

    2oe задание

--- app/src/main/java/com/example/naproject/MainActivity.kt
+++ app/src/main/java/com/example/naproject/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.naproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "TestActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonGoToDetails.setOnClickListener({
+            val rocket = Rocket("Falcon8","8")
+
+            val intent = Intent(this,RocketDetailActivity::class.java)
+            intent.putExtra("Rocket",rocket)
+           // intent.putExtra("Launches","9")
+            startActivity(intent)
+        })
+
+
+        Log.d(TAG,"onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy")
     }
 }
---
+++ app/src/main/java/com/example/naproject/Rocket.kt
@@ -0,0 +1,6 @@
+package com.example.naproject
+
+import java.io.Serializable
+
+data class Rocket( val name: String, val launches: String) : Serializable{
+}
